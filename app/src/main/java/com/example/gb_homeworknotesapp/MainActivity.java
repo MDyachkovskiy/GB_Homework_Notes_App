031a560fdb02818ba787db82440124dfe1ac2902
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2022-11-29 00:55:16 +0500

    макет Main Activity готов

--- app/src/main/java/com/example/gb_homeworknotesapp/MainActivity.java
+++ app/src/main/java/com/example/gb_homeworknotesapp/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.gb_homeworknotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //для инициализации
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout mainLayout = findViewById(R.id.main_layout);
+
+        findViewById(R.id.)
 
         if (savedInstanceState == null) getSupportFragmentManager()
                 .beginTransaction()
