9f4afeafe50146d893c193ed19dd707811365e62
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2022-12-02 13:45:04 +0500

    initial commit

--- app/src/main/java/com/example/gb_homeworknotesapp/MainActivity.java
+++ app/src/main/java/com/example/gb_homeworknotesapp/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         initFragmentLayout(savedInstanceState);
     }
 
+    //ошибки
+
     /*private void initToolbar(){
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
