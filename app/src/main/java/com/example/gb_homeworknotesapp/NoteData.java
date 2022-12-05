c2ad78841843cc33a5a530c6cba84df4b68648b4
Mdyachkovskiy <mdyachkovskiy@gmail.com>
2022-12-05 19:15:25 +0500

    готов макет карточек

--- app/src/main/java/com/example/gb_homeworknotesapp/NoteData.java
+++ app/src/main/java/com/example/gb_homeworknotesapp/NoteData.java
@@ -1,4 +1,26 @@
 package com.example.gb_homeworknotesapp;
 
 public class NoteData {
+    
+    private String title;
+    private String description;
+    private String creationDate;
+
+    public NoteData(String title, String description, String creationDate) {
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
 }
